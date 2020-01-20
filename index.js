1bd254d9b89181ec2fa3041e689ef884d59bcffa
Oliver ALR <oa.lopezrodriguez@ugto.mx>
2020-01-19 23:57:08 -0600

    Up and Up

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
+//::Trying ytdl-core npm module::
+'use strict';
+
 const 
 express = require('express'),
 cors = require('cors'),
 ytdl = require('ytdl-core'),
-fs = require('fs'),
+hbs = require('hbs'),
 app = express();
 
 let port = process.env.PORT || 8080;
 
+hbs.registerPartials('__dirname' + '/src/views');
+app.set('view engine', 'hbs');
+
+//Middleware
 app.use(cors());
- 
+app.use(express.static(__dirname + '/src'));
+
+app.use((req, res) => {
+ res.status(404).render('ERROR 404');
+});
+
+//Routing 
 app.get('/download', (req,res) => {
     let URL = req.query.URL;
 
@@ -18,6 +31,10 @@ app.get('/download', (req,res) => {
     ytdl(URL, {
         format: '.mp4'
     }).pipe(res);
+});
+
+app.get('/', (req, res) => {
+   res.render('index');
 });
 
 app.listen(port, () => console.log(`Server runing at port: ${port}`));
