91ae1511c0097af64052ee259684aee6c30a92f5
Oliver ALR <oa.lopezrodriguez@ugto.mx>
2020-01-21 23:53:49 -0600

    Updated!

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
+class Convert {
+    constructor(URL, metadata = 'just a temp. string'){
+        this.URL = URL;
+        this.metadata = metadata;
+    }
+    
+    get urlStatus(){
+        return `Actual URL: ${this.URL} | Metadata: ${this.metadata}`
+    }
+    
+    static sendURL(URL){
+        window.location.href = `http://localhost:8080/download?URL=${this.URL}`;
+    }
+}
+
 const downloadBtn = document.querySelector('.convert-button');
 const URLinput = document.querySelector('.URL-input');
 
+let convert = new Convert(URLinput.value);
+
 downloadBtn.addEventListener('click', () => {
     console.log(`URL: ${URLinput.value}`);
-    sendURL(URLinput.value);
+    //This is the static method that sends the query string trough the server
+    Convert.sendURL(URLinput.value);
 });
 
-//This is the function that sends the query string trough the server
-const sendURL = (URL) => {
-    window.location.href = `http://localhost:8080/download?URL=${URL}`;
-};
